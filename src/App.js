39a12c87d5891b528fc0121ac8848b4896db02c5
francorosa <km115.franco@gmail.com>
2022-01-11 15:44:27 -0500

    Test later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [hidden, setHidden] = useState(false);
+
+  const handleEnter = () => {
+    setHidden(true);
+  };
+
+  useEffect(() => {
+    const refresh = setInterval(() => {
+      setHidden(false);
+    }, 5000);
+
+    return () => clearInterval(refresh);
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" style={{ display: hidden ? "none" : "block" }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Golfstöðin</h1>
+        <p>Enter your reservation code to unlock:</p>
+        <input
+          placeholder="******"
+          type="number"
+          style={{
+            border: "none",
+            backgroundColor: "transparent",
+            color: "white",
+            fontSize: "2em",
+            textAlign: "center",
+            borderRadius: "15px",
+          }}
+        />
+        <button
+          style={{
+            margin: ".5em",
+            fontSize: "2em",
+            borderRadius: "15px",
+            backgroundColor: "transparent",
+            color: "white",
+          }}
+          onClick={handleEnter}
         >
-          Learn React
-        </a>
+          Enter
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
